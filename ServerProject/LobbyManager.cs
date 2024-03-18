247d5e855facc3a54b6925c58b569b8eb63eec23
SilverMoon <suhongbo0725@gmail.com>
2024-03-18 11:34:29 +0800

    反射改委托进行加速，UniqueID二次校验，动态加载卸载新的大厅类型，增加新的MainLobby通讯协议，完善MainLobby通讯的处理

--- ServerProject/LobbyManager.cs
+++ ServerProject/LobbyManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,8 @@ namespace ServerProject
     internal static class LobbyManager
     {
         static Dictionary<string, Lobby> _lobbys = [];
-        internal static Dictionary<string, Type> _lobbyConstructor = [];
+        internal static Dictionary<string, Delegate> _lobbyConstructor = [];
+        static Dictionary<string,string> _externalLobbyType = [];
         internal static bool TryCreate(string[] createParamters, [MaybeNullWhen(false)] out Lobby lobby)
         {
             lock (_lobbys)
@@ -25,11 +27,6 @@ namespace ServerProject
                     }
                     _params[pair[0]] = pair[1];
                 }
-                if (!_params.TryGetValue("Type", out string lobbyType) || !_lobbyConstructor.TryGetValue(lobbyType, out Type type))
-                {
-                    lobby = null;
-                    return false;
-                }
                 if (_params.TryGetValue("ID", out string uniqueID))
                 {
                     if (_lobbys.TryGetValue(uniqueID, out lobby))
@@ -37,13 +34,74 @@ namespace ServerProject
                         return false;
                     }
                 }
-                lobby = (Lobby?)(type.GetConstructor([typeof(Dictionary<string, string>)])?.Invoke([_params]));
-                return lobby is not null;
+                if (!_params.TryGetValue("Type", out string lobbyType) || !_lobbyConstructor.TryGetValue(lobbyType, out var createMethod))
+                {
+                    lobby = null;
+                    return false;
+                }
+                _params["ID"] = Guid.NewGuid().ToString();
+                lobby = createMethod.DynamicInvoke(_params) as Lobby;
+                if(lobby is not null)
+                {
+                    if (_lobbys.TryAdd(lobby.UniqueID, lobby))
+                    {
+                        lobby.Init();
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
             }
         }
         internal static bool TryFindLobby(string uniqueID, [MaybeNullWhen(false)] out Lobby lobby)
         {
             return _lobbys.TryGetValue(uniqueID, out lobby) && !(lobby?.IsClosed ?? true);
+        }
+        internal static List<string> TryLoadNewLobbyTypes(string secretKey, byte[] assemblyStream)
+        {
+            Assembly assembly = Assembly.Load(assemblyStream);
+            List<string> successLoad = [];
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.IsSubclassOf(typeof(Lobby)) && !type.IsAbstract)
+                {
+                    var constructor = type.GetConstructor([typeof(Dictionary<string, string>)]);
+                    if (constructor != null && type.FullName != null && !_lobbyConstructor.ContainsKey(type.FullName))
+                    {
+                        var method = type.GetMethod("TryCreate", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic, [typeof(Dictionary<string, string>)]);
+                        if(method == null)
+                        {
+                            continue;
+                        }
+                        if (method.ReturnType != typeof(Lobby))
+                        {
+                            continue;
+                        }
+                        _lobbyConstructor.Add(type.FullName, Delegate.CreateDelegate(typeof(Func<Dictionary<string, string>, Lobby>), method));
+                        _externalLobbyType.Add(type.FullName, secretKey);
+                        successLoad.Add(type.FullName);
+                    }
+                }
+            }
+            return successLoad;
+        }
+
+        internal static bool TryRemove(string secretKey, string lobbyType,[MaybeNullWhen(true)] out string failReason)
+        {
+            if(!_externalLobbyType.TryGetValue(lobbyType,out string secretKeyCheck))
+            {
+                failReason = "Lobby Type  does not exist";
+                return false;
+            }
+            if(secretKeyCheck!=secretKey)
+            {
+                failReason = "SecretKey mismatch";
+                return false;
+            }
+            _lobbyConstructor.Remove(lobbyType);
+            _externalLobbyType.Remove(lobbyType);
+            failReason = null;
+            return true;
         }
     }
 }
--- ServerProject/MainLobby.cs
+++ ServerProject/MainLobby.cs
@@ -13,7 +13,9 @@ namespace ServerProject
         enum PacketType : int
         {
             FindLobby,
-            CreateLobby
+            CreateLobby,
+            AddNewLobbyType,
+            RemoveLobbyType
         }
         public MainLobby(string uniqueID, string? passWord, int ipv4Port, int ipv6Port)
         {
@@ -29,72 +31,20 @@ namespace ServerProject
             using BinaryReader reader = new(memory);
             using MemoryStream reply = new();
             using BinaryWriter writer = new(reply);
-            PacketType packetType = (PacketType)reader.ReadInt32();
-            switch (packetType)
+            string? replyDialogue = null;
+            try
             {
-                case PacketType.FindLobby:
-                    {
-                        if (LobbyManager.TryFindLobby(reader.ReadString(), out Lobby lobby))
+                replyDialogue = reader.ReadString();
+                writer.Write(replyDialogue);
+                writer.Write("Lobby");
+                PacketType packetType = (PacketType)reader.ReadInt32();
+                switch (packetType)
+                {
+                    case PacketType.FindLobby:
                         {
-                            writer.Write(true);
-                            writer.Write(lobby.UniqueID);
-                            switch (socket.RemoteAddress.Type)
+                            if (LobbyManager.TryFindLobby(reader.ReadString(), out Lobby lobby))
                             {
-                                case RemoteAddress.AddressType.IPV4:
-                                    {
-                                        writer.Write(lobby.IPV4Port);
-                                        break;
-                                    }
-                                case RemoteAddress.AddressType.IPV6:
-                                    {
-                                        writer.Write(lobby.IPV6Port);
-                                        break;
-                                    }
-                                case RemoteAddress.AddressType.NamedPipeStream:
-                                    {
-                                        writer.Write(lobby.LocalServerName);
-                                        break;
-                                    }
-                            }
-                        }
-                        else
-                        {
-                            writer.Write(false);
-                        }
-                        break;
-                    }
-                case PacketType.CreateLobby:
-                    {
-                        string[] createParamters = reader.ReadString().Split(" ");
-                        if (LobbyManager.TryCreate(createParamters, out Lobby lobby))
-                        {
-                            lobby.Init();
-                            writer.Write(0);
-                            writer.Write(lobby.UniqueID);
-                            switch (socket.RemoteAddress.Type)
-                            {
-                                case RemoteAddress.AddressType.IPV4:
-                                    {
-                                        writer.Write(lobby.IPV4Port);
-                                        break;
-                                    }
-                                case RemoteAddress.AddressType.IPV6:
-                                    {
-                                        writer.Write(lobby.IPV6Port);
-                                        break;
-                                    }
-                                case RemoteAddress.AddressType.NamedPipeStream:
-                                    {
-                                        writer.Write(lobby.LocalServerName);
-                                        break;
-                                    }
-                            }
-                        }
-                        else
-                        {
-                            if (lobby is not null)
-                            {
-                                writer.Write(1);
+                                writer.Write(true);
                                 writer.Write(lobby.UniqueID);
                                 switch (socket.RemoteAddress.Type)
                                 {
@@ -117,11 +67,115 @@ namespace ServerProject
                             }
                             else
                             {
-                                writer.Write(2);
+                                writer.Write(false);
                             }
+                            break;
                         }
-                        break;
-                    }
+                    case PacketType.CreateLobby:
+                        {
+                            string[] createParamters = reader.ReadString().Split(" ");
+                            if (LobbyManager.TryCreate(createParamters, out Lobby lobby))
+                            {
+                                writer.Write(0);
+                                writer.Write(lobby.UniqueID);
+                                switch (socket.RemoteAddress.Type)
+                                {
+                                    case RemoteAddress.AddressType.IPV4:
+                                        {
+                                            writer.Write(lobby.IPV4Port);
+                                            break;
+                                        }
+                                    case RemoteAddress.AddressType.IPV6:
+                                        {
+                                            writer.Write(lobby.IPV6Port);
+                                            break;
+                                        }
+                                    case RemoteAddress.AddressType.NamedPipeStream:
+                                        {
+                                            writer.Write(lobby.LocalServerName);
+                                            break;
+                                        }
+                                }
+                            }
+                            else
+                            {
+                                if (lobby is not null)
+                                {
+                                    writer.Write(1);
+                                    writer.Write(lobby.UniqueID);
+                                    switch (socket.RemoteAddress.Type)
+                                    {
+                                        case RemoteAddress.AddressType.IPV4:
+                                            {
+                                                writer.Write(lobby.IPV4Port);
+                                                break;
+                                            }
+                                        case RemoteAddress.AddressType.IPV6:
+                                            {
+                                                writer.Write(lobby.IPV6Port);
+                                                break;
+                                            }
+                                        case RemoteAddress.AddressType.NamedPipeStream:
+                                            {
+                                                writer.Write(lobby.LocalServerName);
+                                                break;
+                                            }
+                                    }
+                                }
+                                else
+                                {
+                                    writer.Write(2);
+                                }
+                            }
+                            break;
+                        }
+                    case PacketType.AddNewLobbyType:
+                        {
+                            string secretKey = reader.ReadString();
+                            int assemblyLength = reader.ReadInt32();
+                            var list = LobbyManager.TryLoadNewLobbyTypes(secretKey, reader.ReadBytes(assemblyLength));
+                            if(list.Count>0)
+                            {
+                                writer.Write(true);
+                                writer.Write(list.Count);
+                                list.ForEach(writer.Write);
+                            }
+                            else
+                            {
+                                writer.Write(false);
+                            }
+                            break;
+                        }
+                    case PacketType.RemoveLobbyType:
+                        {
+                            string secretKey = reader.ReadString();
+                            string lobbyType = reader.ReadString();
+                            if(LobbyManager.TryRemove(secretKey, lobbyType,out string failReason))
+                            {
+                                writer.Write(true);
+                            }
+                            else
+                            {
+                                writer.Write(false);
+                                writer.Write(failReason);
+                            }
+                            break;
+                        }
+                }
+                socket.AsyncSend(reply.ToArray());
+            }
+            catch (Exception ex)
+            {
+                writer.Write(replyDialogue ?? "UnknowDialogue");
+                writer.Write("Lobby");
+                writer.Write(ex.ToString());
+                socket.AsyncSend(reply.ToArray());
+            }
+            finally
+            {
+                writer.Dispose();
+                reply.Dispose();
+                memory.Dispose();
             }
         }
     }
